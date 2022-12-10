9a72d37417cca7ed1ba614a3999d921a70636851
Gviswesh <visweshsubramanian@outlook.com>
2022-12-10 08:06:27 +0530

    The layouts have been changed and 2 textviews have been added

--- app/src/main/java/com/example/layoutmanager/MainActivity.java
+++ app/src/main/java/com/example/layoutmanager/MainActivity.java
@@ -11,31 +11,40 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-    EditText e1,e2;
+    EditText e1,e2,e3;
     Button bt;
-    Spinner s;
+    Spinner s,y;
     String[] dept_array={"CSE","ECE","IT","MECHANICAL","CIVIL"};
-    String name,reg,dept;
+    Integer[] year_array={1998,1999,2000,2001,2002};
+    String name,reg,edu,yearof,dept;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         e1=findViewById(R.id.editText1);
         e2=findViewById(R.id.editText2);
+        e3=findViewById(R.id.editText3);
         bt=findViewById(R.id.button);
-        s=findViewById(R.id.spinner);
+        s=findViewById(R.id.dept_spinner);
+        y=findViewById(R.id.age_spinner);
         ArrayAdapter adapter=new ArrayAdapter(MainActivity.this, android.R.layout.simple_spinner_item,dept_array);
+        ArrayAdapter year=new ArrayAdapter(MainActivity.this, android.R.layout.simple_spinner_item,year_array);
         s.setAdapter(adapter);
+        y.setAdapter(year);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 name=e1.getText().toString();
                 reg=e2.getText().toString();
+                edu=e3.getText().toString();
                 dept=s.getSelectedItem().toString();
+                yearof=y.getSelectedItem().toString();
                 Intent i= new Intent(MainActivity.this,Second.class);
                 i.putExtra("name_key",name);
                 i.putExtra("reg_key",reg);
                 i.putExtra("dept_key",dept);
+                i.putExtra("year_key",yearof);
+                i.putExtra("edu_key",edu);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/layoutmanager/Second.java
+++ app/src/main/java/com/example/layoutmanager/Second.java
@@ -7,8 +7,8 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 public class Second extends AppCompatActivity {
-    TextView t1,t2,t3;
-    String name,reg,dept;
+    TextView t1,t2,t3,t4,t5;
+    String name,reg,dept,edu,year;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,13 +16,19 @@ public class Second extends AppCompatActivity {
         t1=findViewById(R.id.textView1);
         t2=findViewById(R.id.textView2);
         t3=findViewById(R.id.textView3);
+        t4=findViewById(R.id.textView4);
+        t5=findViewById(R.id.textView5);
         Intent i=getIntent();
          name=i.getStringExtra("name_key");
          reg=i.getStringExtra("reg_key");
+         edu=i.getStringExtra("edu_key");
+         year=i.getStringExtra("year_key");
          dept=i.getStringExtra("dept_key");
          t1.setText(name);
          t2.setText(reg);
-         t3.setText(dept);
+         t3.setText(edu);
+         t4.setText("YEAR OF BIRTH:"+year);
+         t5.setText(dept);
 
 
     }
